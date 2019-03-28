eee474f7d7b26fa80b25872c92302384ea06bd92
vali201232 <v.kellermair@gmail.com>
2019-03-28 12:00:01 +0100

    Signup testing

--- app/src/main/java/com/example/vkellermair16_brwtalk/MainActivity.java
+++ app/src/main/java/com/example/vkellermair16_brwtalk/MainActivity.java
@@ -1,18 +1,95 @@
 package com.example.vkellermair16_brwtalk;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+    private static final String TAG = "MainActivity";
+    ListView listView;
+    Button button;
+    TextView textView;
+    String endString;
+    ArrayList<String> arrayList = new ArrayList<>();
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        FirebaseFirestore db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ArrayAdapter<String> itemsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, arrayList);
+
+        mAuth = FirebaseAuth.getInstance();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+
+        editText = findViewById(R.id.editText);
+        button = findViewById(R.id.button);
+        listView = findViewById(R.id.listView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                buttonMethodStuff();
+            }
+        });
 
 
     }
+
+    public void buttonMethodStuff() {
+        String string = editText.getText().toString();
+
+        if (string.startsWith("/login")) {
+
+            String[] stringarr = string.split(" ");
+            final String email = stringarr[1];
+            final String password = stringarr[2];
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(MainActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            // ...
+                        }
+                    });
+
+        } else {
+
+            endString = string;
+        }
+
+    }
+
 }
