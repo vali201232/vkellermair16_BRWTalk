f957df9d4ab7862789d88105c1a4de6a56007956
vali201232 <v.kellermair@gmail.com>
2019-03-28 09:34:55 +0100

    Welschcode für Firebase und Layout

--- app/src/main/java/com/example/vkellermair16_brwtalk/MainActivity.java
+++ app/src/main/java/com/example/vkellermair16_brwtalk/MainActivity.java
@@ -3,11 +3,16 @@ package com.example.vkellermair16_brwtalk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.firestore.FirebaseFirestore;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
 }
